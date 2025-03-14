67dca3df14ff69f51c2e99f9fe0fc083216d739c
GersonESS <gebhsantos@gmail.com>
2025-03-14 20:18:41 -0300

    a

--- 04base/sitebase/src/App.js
+++ 04base/sitebase/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Topo from './componentes/Topo';
+import Rodape from './componentes/Rodape';
 
 function App() {
   return (
     <>
      <Topo />
+     <Rodape />
     
     </>
   );
