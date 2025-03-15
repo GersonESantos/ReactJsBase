cd1dce67816bdfe81f3d265990689dcdde0fe07f
GersonESS <gebhsantos@gmail.com>
2025-03-15 02:02:35 -0300

    j

---
+++ curso/05curso/src/App.jsx
@@ -0,0 +1,12 @@
+import Tasks from "./components/Tasks";
+function App() {
+  
+  
+  return (
+    <div>
+      <h1>Gerenciador De Tarefas</h1>
+      <Tasks />
+    </div>
+  );
+}
+export default App;
