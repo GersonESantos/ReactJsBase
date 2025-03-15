2a157d5ab59d8e007c4027a1e0d75470057c4bda
GersonESS <gebhsantos@gmail.com>
2025-03-15 01:38:16 -0300

    Update App.jsx

--- curso/03curso/src/App.jsx
+++ curso/03curso/src/App.jsx
@@ -1,9 +1,19 @@
+import { useState} from "react";
 function App() {
- 
+  const [message, setMessage] = useState("Ola Mundo");
+  
   return (
     <div>
       <h1>React Basico</h1>
-      <p>Este é curso de React</p>
+      <p>{message}</p>
+      <button 
+        onClick={() => {
+        setMessage ("Mensagem alterada");
+        
+      }}
+      >
+        Mudar Messagem 
+      </button>
     </div>
   );
 }
