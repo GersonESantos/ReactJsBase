a74ca12883e3d9adb11fd7fae11e6d5812624c46
GersonESS <gebhsantos@gmail.com>
2025-03-15 23:17:29 -0300

    Update App.jsx

--- Sujeito/01s/src/App.jsx
+++ Sujeito/01s/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import UserForm from './components/UserForm';
 import './App.css'
 
 function App() {
   
-
+  function handleAddUser(formData) {
+    const nome = formData.get('nome');
+    console.log(nome);
+    const sobrenome = formData.get('sobrenome');
+    console.log(sobrenome);
+    const email = formData.get('email');
+    console.log(email);
+    const senha = formData.get('senha');
+    console.log(senha);
+    const confirmaSenha = formData.get('confirmaSenha');
+    console.log(confirmaSenha);
+  }
   return <>
-      <h1>01-Actions</h1>
-      <UserForm />
+
+      <form action={handleAddUser} method="post">
+        <input type="text" name="nome" id="nome" />
+        <input type="text" name="sobrenome" id="sobrenome" />
+        <input type="text" name="email" id="email" />
+        <input type="text" name="senha" id="senha" />
+        <input type="text" name="confirmaSenha" id="confirmaSenha" />
+        <button type="submit">Enviar</button>
+      </form>
       
     </> 
 };
