d5d9bb63ec5dd3b020d8d281988b06ed50861011
GersonESS <gebhsantos@gmail.com>
2025-03-16 18:34:48 -0300

    a

---
+++ reacj19/07r/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import UserForm from './components/UserForm';
+
+function App() { 
+  return (   
+    <>
+    <h1>Hora de Codar</h1>
+
+      <h2>1 - Actions</h2>
+      <UserForm />
+      
+    </> 
+    );
+};
+
+export default App;
