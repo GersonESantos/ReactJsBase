18765c4cecad082f590d7bbe394d32e0b7af6dad
GersonESS <gebhsantos@gmail.com>
2025-03-16 16:02:08 -0300

    aa

---
+++ TreinaWeb/01t/src/App.jsx
@@ -0,0 +1,14 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+function App() {
+  
+  
+  return (
+    <div>
+      <h1>Gerenciador De Tarefas</h1>
+      <Tasks />
+      <AddTask />
+    </div>
+  );
+}
+export default App;
