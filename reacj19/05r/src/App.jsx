6cdf095ef04fe5ded246cb1907688cacb03b5b2e
GersonESS <gebhsantos@gmail.com>
2025-03-16 17:17:50 -0300

    a

---
+++ reacj19/05r/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import UserForm from './components/UserForm';
+import './App.css'
+
+function App() {
+  
+
+  return <>
+      <h1>01-Actions</h1>
+      <UserForm />
+      
+    </> 
+};
+
+export default App;
