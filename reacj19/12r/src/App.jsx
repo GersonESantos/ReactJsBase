15dd9efd537c51afe1c7bab57517df779a73be67
GersonESS <gebhsantos@gmail.com>
2025-03-16 23:03:18 -0300

    a

---
+++ reacj19/12r/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './components/UserForm';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importe aqui também, se preferir centralizar
+
+function App() {
+    return (
+        <div className="container text-center">
+            {/* <h1 className="my-4">Hora de Codar</h1> */}
+            <h2 className="mb-4">TODO Lista</h2>
+            <UserForm />
+        </div>
+    );
+}
+
+export default App;
