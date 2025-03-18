3af0994cb98340429395a39584cb590253bf80da
GersonESS <gebhsantos@gmail.com>
2025-03-18 18:29:32 -0300

    a

---
+++ react19v2/08r/src/App.jsx
@@ -0,0 +1,16 @@
+import UserForm from './components/UserForm';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importe aqui também, se preferir centralizar
+
+import './App.css'
+
+function App() {
+  
+
+  return <>
+      
+      <UserForm />
+      
+    </> 
+};
+
+export default App;
