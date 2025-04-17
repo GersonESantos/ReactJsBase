e4de7c6acef80f3a95c1eb1e66bf2ee42d8efc32
GersonESS <gebhsantos@gmail.com>
2025-04-17 16:48:35 -0300

    a

---
+++ hooks/useEffect/03useeffect/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [like, setLike] = useState(0)
+  function darLike() {
+    setLike(like + 1)
+  }
+
+
+useEffect(() => {
+  if (like > 9) {
+  alert(`useEffect rodou voce passou de ${like} likes`)
+}
+})
+
+  return (
+    <>
+      <div>
+        <p>Quantidade de like</p>
+        <button onClick={darLike}>Dar like</button>
+        <p>{like}</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
