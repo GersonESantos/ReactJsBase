c26d36bf486a03ba37f53622ae368dfc7b6c4349
GersonESS <gebhsantos@gmail.com>
2025-04-21 14:06:40 -0300

    a

---
+++ celR19/03c/meu/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+const  About = () =>  {
+    return (
+      <div>
+        <h1>Menu</h1>
+        <h2>Sobre</h2>
+        
+      </div>
+     
+    );
+  }
+  export default About;
---
+++ celR19/03c/meu/src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+
+const  Contact = () =>  {
+    return (
+      <div>
+        <h1>Menu</h1>
+        <h2>Contato</h2>
+        
+      </div>
+     
+    );
+}
+  export default Contact;
