950d3708b040be77bd18df6ef5646d692024cc06
GersonESS <gebhsantos@gmail.com>
2025-04-21 00:02:06 -0300

    a

---
+++ celR19/05c/meu/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
+const  About = () =>  {
+    return (
+      <div>
+        <Menu />
+        <h2>Sobre a Empresa</h2>
+        <Footer />
+      </div>
+     
+    );
+  }
+  export default About;
---
+++ celR19/05c/meu/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Menu from "@/components/Menu";
+const  Home = () =>  {
+  return (
+    <div>
+      <Menu /><br />
+      <h2>Bem vindo GersonES</h2><br />
+      <Footer />
+    </div>
+   
+  );
+}
+export default Home;
