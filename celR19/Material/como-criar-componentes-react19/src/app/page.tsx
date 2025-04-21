4a97ae69a3e8cbaaa8467b1ca63806381342eb5e
GersonESS <gebhsantos@gmail.com>
2025-04-20 22:53:35 -0300

    s

---
+++ celR19/Material/como-criar-componentes-react19/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Menu /><br />
+      <h2>Bem-vindo Celke!</h2><br />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
