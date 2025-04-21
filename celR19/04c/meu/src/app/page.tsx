e6ef38e1e614b903774936caf7c4f5eebb2cdf9a
GersonESS <gebhsantos@gmail.com>
2025-04-20 22:52:32 -0300

    e

---
+++ celR19/04c/meu/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Menu from "@/components/Menu";
+const  Home = () =>  {
+  return (
+    <div>
+      <Menu />
+      <h2>Bem vindo GersonES</h2>
+    </div>
+   
+  );
+}
+export default Home;
---
+++ celR19/04c/meu/src/components/menu/index.tsx
@@ -0,0 +1,15 @@
+const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/a">Home</a></li>
+                <li><a href="/about">Sobre</a></li>
+                <li><a href="/contact">Contato</a></li>
+                
+            </ul>
+        <h1>Menu</h1>
+        <h2>Bem vindo GersonES</h2>
+        </nav>
+    );
+    }
+    export default Menu;
