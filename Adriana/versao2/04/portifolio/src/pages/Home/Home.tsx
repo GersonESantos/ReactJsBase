8cddd93fc9b3b4a53ec8009ce775cad177c1cfa3
GersonESS <gebhsantos@gmail.com>
2025-08-03 02:13:23 -0300

    a

--- Adriana/versao2/04/portifolio/src/pages/Home/Home.tsx
+++ Adriana/versao2/04/portifolio/src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
+
 import Hero from "./sections/Hero/Hero"
 
-const Home = () => {  
 
-  return (
-    <>
-        <Hero/>
-    </>
-  )
+const Home = () => {
+
+
+    return (
+        <>
+            
+            <Hero />
+        </>
+    )
 }
 
 export default Home
