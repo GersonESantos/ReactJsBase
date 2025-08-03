22a6b15cdc8966bce688bf9ecad3e7728dee75f8
GersonESS <gebhsantos@gmail.com>
2025-08-03 16:56:12 -0300

    a

--- Adriana/02ver/02/portifolio/src/pages/Home/sections/Hero/Hero.tsx
+++ Adriana/02ver/02/portifolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material";
-
+import Avatar from "../../../../assets/images/GersonES2025.jpg"
 interface HeroProps {
   theme: string;
 }
@@ -16,7 +16,12 @@ const Hero: React.FC<HeroProps> = ({ theme }) => {
         color: theme === 'light' ? "black" : "white",
         transition: "all 0.3s ease"
     }));  
-
+const StyledImg = styled("img")(() => ({   
+        width: "75%",
+        borderRadius: "50%",
+        height: "300px", // Mantém proporção circular
+        objectFit: "cover", // Garante que a imagem se ajuste bem
+    }));
     return (
         <StyledHero>
             <h1>Welcome to My Portfolio</h1>
