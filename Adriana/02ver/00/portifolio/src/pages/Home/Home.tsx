e79c505ddecbd3694e7aedcf069db377e45077c0
GersonESS <gebhsantos@gmail.com>
2025-08-04 00:40:07 -0300

    a

--- Adriana/02ver/00/portifolio/src/pages/Home/Home.tsx
+++ Adriana/02ver/00/portifolio/src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import Hero from "./sections/Hero/Hero"
-
 import NavBar from "../../components/NavBar/NavBar"
-const Home = () => {  
 
+interface HomeProps {
+  theme: string;
+}
+
+const Home: React.FC<HomeProps> = ({ theme }) => {  
   return (
     <>
-        <Hero />
+        <Hero theme={theme} />
         <NavBar />
     </>
   )
